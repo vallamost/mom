e490eb3a02c6a3eb155c7036854e91ae2dec7561
Ian Smith <mclovin@Ians-MacBook-Air.local>
2021-07-12 01:40:09 -0700

    bug fixes and improvements

--- indexCreator.py
+++ indexCreator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """ Build index from directory listing
 
 make_index.py </path/to/directory> [--header <header text>]
--- startDockerWebServer.sh
+++ startDockerWebServer.sh
@@ -2,4 +2,6 @@
 
 #docker build -t webserver .
 
-sudo docker run --name webserver -p 80:80 -d -v ./:/usr/share/nginx/html
+#sudo docker run webserver -p 80:80 -d -v ./:/usr/share/nginx/html
+
+sudo docker run -d -p 80:80 -v "/$(pwd):/usr/share/nginx/html" webserver
