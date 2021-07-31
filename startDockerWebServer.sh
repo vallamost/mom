6e6be96a75ef172e61ac5e9abd69b5ad1c51f5e3
Ian S <vallamost@gmail.com>
2021-07-31 12:44:40 -0700

    Adding more songs

--- startDockerWebServer.sh
+++ startDockerWebServer.sh
@@ -2,6 +2,4 @@
 
 #docker build -t webserver .
 
-#sudo docker run webserver -p 80:80 -d -v ./:/usr/share/nginx/html
-
-sudo docker run -d -p 80:80 -v "/$(pwd):/usr/share/nginx/html" webserver
+docker run -it --rm -p 80:80 -d -v $(pwd):/usr/share/nginx/html --name web webserver
